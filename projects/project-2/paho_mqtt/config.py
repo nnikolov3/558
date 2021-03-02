9307f3fcf1c38b46cbeb875aac0d9a87bfdd847f
nnikolov3 <nnikolov@pdx.edu>
2021-03-02 08:52:32 -0800

    nvim init

---
+++ projects/project-2/paho_mqtt/config.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# --------------------------------
+# Setting up the
+# the configuration values
+# to establish a connection with Mosquitto
+# --------------------------------

8c404776ab442f5b718d480021081b5c3a6227f7
nnikolov3 <nnikolov@pdx.edu>
2021-03-10 09:13:38 -0800

    "added checkstyle"

---
+++ java-jar-checkstyle.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+filename="$1"
+extension="${filename##*.}"
+if [ "$extension" == ".java" ]; then
+    cmd="java -jar "$HOME"/checkstyle-8.41-all.jar -c "$HOME"/google_checks.xml"
+    exec "$cmd" $filename
+else
+    echo "Pass ..."
+fi
+exit 0

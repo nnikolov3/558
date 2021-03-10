f04d1f8340f1ba6b53f4d0f37b81cc8546ec40a1
nnikolov3 <nnikolov@pdx.edu>
2021-03-10 10:03:37 -0800

    "formatting and linting"

--- java-linting/java-jar-checkstyle.sh
+++ java-linting/java-jar-checkstyle.sh
@@ -2,9 +2,8 @@
 
 filename="$1"
 extension="${filename##*.}"
-if [ "$extension" == ".java" ]; then
-    cmd="java -jar "$HOME"/checkstyle-8.41-all.jar -c "$HOME"/google_checks.xml"
-    exec "$cmd" $filename
+if [ "$extension" == "java" ]; then
+    java -jar "$HOME"/checkstyle-8.41-all.jar -c "$HOME"/google_checks.xml
 else
     echo "Pass ..."
 fi
--- project-2/app/src/main/java/com/mqtt/paho/ui/main/MainFragment.java
+++ project-2/app/src/main/java/com/mqtt/paho/ui/main/MainFragment.java
@@ -1,26 +1,14 @@
-/*
- *  -----------------------------
- * Author: nnikolov3
- * Date: 03/09/2021
- *  -----------------------------
- * Copyright (c) 2021
- *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
+/**
+ * Author: nnikolov3 Date: 03/09/2021 Copyright (c) 2021 This program is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version. This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE. See the GNU General Public License for more details. You should have received a copy of
+ * the GNU General Public License along with this program. If not, https://www.gnu.org
  */
-
 package com.mqtt.paho.ui.main;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -33,58 +21,54 @@ import androidx.lifecycle.ViewModelProvider;
 import mqtt.paho.R;
 import mqtt.paho.databinding.MainFragmentBinding;
 
-
+/** The main fragments updates the ui. */
 public final class MainFragment extends Fragment {
 
-    private MainViewModel mViewModel;
-    private LayoutInflater inflater;
-    @Nullable
-    private ViewGroup container;
-    @Nullable
-    private Bundle savedInstanceState;
+  private MainViewModel mySensorViewModel;
+  private LayoutInflater inflater;
+  @Nullable private ViewGroup container;
+  @Nullable private Bundle savedInstanceState;
 
-    private MainFragment() {
-    }
+  private MainFragment() {}
 
-    public static MainFragment newInstance() {
-        return createMainFragment();
-    }
+  public static MainFragment newInstance() {
+    return createMainFragment();
+  }
 
-    public static MainFragment createMainFragment() {
-        return new MainFragment();
-    }
+  public static MainFragment createMainFragment() {
+    return new MainFragment();
+  }
 
+  // -----------------------------
+  @Nullable
+  @Override
+  public View onCreateView(
+      @NonNull final LayoutInflater inflater,
+      @Nullable final ViewGroup container,
+      @Nullable final Bundle savedInstanceState) {
+    this.inflater = inflater;
+    this.container = container;
+    this.savedInstanceState = savedInstanceState;
+
+    // Creating the data binding
     // -----------------------------
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull final LayoutInflater inflater,
-        @Nullable final ViewGroup container,
-        @Nullable final Bundle savedInstanceState) {
-        this.inflater = inflater;
-        this.container = container;
-        this.savedInstanceState = savedInstanceState;
+    final MainFragmentBinding binding;
+    // Reference to the binding class
+    binding = DataBindingUtil.inflate(inflater, R.layout.main_fragment, container, false);
 
-        // Creating the data binding
-        // -----------------------------
-        final MainFragmentBinding binding;
-        // Reference to the binding class
-        binding = DataBindingUtil.inflate(
-            inflater, R.layout.main_fragment, container, false);
+    /* Ensure that the instance is destroyed
+       when the fragment goes away
+    */
+    binding.setLifecycleOwner(this);
+    final View view = binding.getRoot();
+    binding.setVariable(uiController, this);
+    return view;
+  }
 
-        /* Ensure that the instance is destroyed
-            when the fragment goes away
-         */
-        binding.setLifecycleOwner(this);
-        final View view = binding.getRoot();
-        binding.setVariable(uiController, this);
-        return view;
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable final Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        final ViewModelProvider viewModelProvider = new ViewModelProvider(this);
-        this.mViewModel = viewModelProvider.get(MainViewModel.class);
-    }
-
+  @Override
+  public void onActivityCreated(@Nullable final Bundle savedInstanceState) {
+    super.onActivityCreated(savedInstanceState);
+    final ViewModelProvider viewModelProvider = new ViewModelProvider(this);
+    this.mySensorViewModel = viewModelProvider.get(MainViewModel.class);
+  }
 }
